3825cbf4f8a85e8b67beb06fde9059f6bfd60a89
perugusruthi <perugu.sruthi@gmail.com>
2024-10-19 10:21:28 +0100

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,8 @@
+const amount=9;
+if(amount<10){
+    console.log("Amount is small");
+}else{
+    console.log("Amount is Large");
+}
+
+console.log("Its my First program for today");
---
+++ 3-modules.js
@@ -0,0 +1,7 @@
+const names = require("./4-names")
+const sayHi = require("./5-utils")
+const data = require('./6-alternative-flavour')
+require('./7-mind-grenade')
+sayHi("sruthi")
+sayHi(names.john)
+sayHi(names.peter)
